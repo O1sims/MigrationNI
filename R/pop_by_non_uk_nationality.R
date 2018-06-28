dbeac6255615ffa3e1a567a4c0f54c3e94fd8e62
Owen Sims <owen.sims@cyberlytic.com>
2018-06-28 21:29:38 +0100

    Update population change by non-UK nationality

--- R/pop_by_non_uk_nationality.R
+++ R/pop_by_non_uk_nationality.R
@@ -1,38 +1,78 @@
-# Population by non-UK nationality, Northern Ireland, year ending June 2017
+#' @title Population breakdown by non-UK nationality, Northern Ireland, year ending June 2017
+#' 
+#' @description A donut chart showing the break-down of non-UK nationalities in NI. 
+#'     This chart is representative of Page 7 of the Scotland document.
+#'     
+#' @source 
 
 
 library(ggplot2)
+library(ggthemes)
 library(magrittr)
 
 
-browser <- c(rep("EU", 4), rep("Non-EU", 3))
-version <- c("EU8", "EU14", "EU2", "EU Other", "Other Europe", "Rest of the world", "Asia")
-share <- c(54, 37, 6, 0, 1, 8, 17)
-ymax <- ymin <- c()
-for (i in 1:length(share)) {
-  if (i == 1) {
-    ymax %<>% append(share[i])
-    ymin[i] <- 0
-  } else {
-    ymax %<>% append(share[i] + ymax[i-1])
-    ymin[i] <- ymax[i-1]
-  }
+populationBreakdownNonUK <- function() {
+  locations <- data.frame(
+    area = c(
+      rep("EU", 4), 
+      rep("Non-EU", 3)),
+    partition = c(
+      "EU8", "EU14", "EU2", "EU Other", 
+      "Other Europe", "Rest of the world", "Asia"),
+    share = c(
+      54, 37, 6, 0, 1, 8, 17),
+    stringsAsFactors = FALSE)
+  
+  locations$ymax <- locations$share %>% cumsum()
+  locations$ymin <- c(0, head(locations$ymax, n = -1))
+  
+  breakdownPlot <- ggplot2::ggplot(locations) +
+    xlim(c(0, 5.5)) + 
+    geom_rect(
+      aes(fill = partition, 
+          ymax = ymax, ymin = ymin, 
+          xmax = 4, xmin = 3)) +
+    geom_rect(
+      aes(fill = area,
+          ymax = ymax, ymin = ymin,
+          xmax = 3, xmin = 2)) + 
+    theme(
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      panel.background = element_blank(),
+      legend.position = "none") +
+    coord_polar(theta = "y") + 
+    ggtitle("Population by non-UK nationality \n year ending June 2017") +
+    scale_fill_ptol()
+  
+  breakdownPlot <- breakdownPlot + 
+    annotate(
+      "text", x = 2.5, y = 50, label = "EU",
+      color = "white", size = 4)  +
+    annotate(
+      "text", x = 2.5, y = 107.5, label = "Non-EU",
+      color = "white", size = 4)  +
+    annotate(
+      "text", x = 3.5, y = 25, label = "EU8",
+      color = "white", size = 4) +
+    annotate(
+      "text", x = 3.5, y = 75, label = "EU14",
+      color = "white", size = 4) +
+    annotate(
+      "text", x = 3.5, y = 94, label = "EU2",
+      color = "white", size = 4) +
+    annotate(
+      "text", x = 3.5, y = 94, label = "EU2",
+      color = "white", size = 4) +
+    annotate(
+      "text", x = 3.5, y = 102, label = "RoW",
+      color = "white", size = 4) +
+    annotate(
+      "text", x = 3.5, y = 113, label = "Asia",
+      color = "white", size = 4) +
+    annotate(
+      "text", x = 5.5, y = 97, label = "Other Europe",
+      size = 4)
+  
+  return(breakdownPlot)
 }
-
-browsers <- data.frame(
-  browser = browser,
-  version = version,
-  share = share,
-  ymax = ymax,
-  ymin = ymin,
-  stringsAsFactors = FALSE)
-
-ggplot(browsers) + 
-  geom_rect(aes(fill=version, ymax=ymax, ymin=ymin, xmax=4, xmin=3)) +
-  geom_rect(aes(fill=browser, ymax=ymax, ymin=ymin, xmax=3, xmin=0)) +
-  xlim(c(0, 4)) + 
-  theme(
-    axis.text = element_blank(),
-    axis.ticks = element_blank(),
-    panel.grid  = element_blank()) +
-  coord_polar(theta="y")
